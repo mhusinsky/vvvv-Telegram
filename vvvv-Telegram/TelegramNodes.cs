778621a74bf2ffa0ddd60c9bb9a79598a6b4e156
Matthias Husinsky <mhusinsky@fhstp.ac.at>
2016-11-21 19:06:13 +0100

    simpler sendMessage node using async calls instead of factory

--- vvvv-Telegram/TelegramNodes.cs
+++ vvvv-Telegram/TelegramNodes.cs
@@ -55,7 +55,7 @@ namespace VVVV.Nodes
         protected Spread<CancellationTokenSource> FCts = new Spread<CancellationTokenSource>();
         protected Spread<CancellationToken> ct = new Spread<CancellationToken>();
         //int TaskCount = 0;
-        readonly Spread<Stopwatch> FStopwatch = new Spread<Stopwatch>();
+        protected Spread<Stopwatch> FStopwatch = new Spread<Stopwatch>();
         
         
         public void OnImportsSatisfied()
@@ -65,94 +65,45 @@ namespace VVVV.Nodes
 
         public void Dispose()
         {
-            // Should this plugin get deleted by the user or should vvvv shutdown
-            // we need to wait until all still running tasks ran to a completion
-            // state.
-            for (int i = 0; i < FTask.SliceCount; i++)
-            {
-                FLogger.Log(LogType.Message, "Dispose task:" + i);
-                CancelRunningTasks(i);
-            }
+
         }
 
 
         public void Evaluate(int SpreadMax)
         {
-            FTask.SliceCount = FClient.SliceCount;
-            FCts.SliceCount = FClient.SliceCount;
-            ct.SliceCount = FClient.SliceCount;
+
             FStopwatch.SliceCount = FClient.SliceCount;
-            
+            FSent.SliceCount = FClient.SliceCount;
+
+
             for (int i=0; i < FClient.SliceCount; i++)
             {
-                if (FCancel[i])
-                {
-                    CancelRunningTasks(i);
-                    FClient[i].BC.StopReceiving();
-                    FStopwatch[i].Stop();
-                    FStopwatch[i].Reset();
-                }
+                if (FStopwatch[i] == null) FStopwatch[i] = new Stopwatch();
+                if(FSent[i]) FSent[i] = false;
 
-                
+
                 if (FSend[i])
                 {
                     if (FClient[i].IsConnected)
                     {
-                        FStopwatch[i] = new Stopwatch();
+                        // STARTTASK
                         FStopwatch[i].Reset();
                         FStopwatch[i].Start();
-                        // STARTTASK
-                        sendMessage(i);
+                        sendMessageAsync(i);
                     }
                     else
                     {
                         FLogger.Log(LogType.Debug, "Bot " + i + ": Cannot Send Text, client not connected");
                     }
                 }
-
-                if (FTask[i] == null)
-                { }
-                else if (FTask[i].Status == TaskStatus.RanToCompletion)
-                {
-                    //FLogger.Log(LogType.Debug, "Task " + i + ": " + FTasks[i].Status.ToString());
-                    if (FStopwatch[i].IsRunning)
-                    {
-                        FStopwatch[i].Stop();
-                        FTime[i] = FStopwatch[i].ElapsedMilliseconds / 1000.0;
-                        FSent[i] = true;
-                    }
-                    else
-                    {
-                        FTime[i] = FStopwatch[i].ElapsedMilliseconds / 1000.0;
-                        FSent[i] = false;
-                    }
-                }
-                else if (FTask[i].Status == TaskStatus.Running)
-                {
-                    //FLogger.Log(LogType.Debug, "Task " + i + ": " + FTasks[i].Status.ToString());
-                }
+                
+                FTime[i] = FStopwatch[i].ElapsedMilliseconds;
             }
 
         }
 
-        protected abstract void sendMessage(int i);
+        protected abstract Task sendMessageAsync(int i);
 
-        // Worker and Helper Methods 
-        private void CancelRunningTasks(int index)
-        {
-            if (FCts[index] != null)
-            {
-                // All our running tasks use the cancellation token of this cancellation
-                // token source. Once we call cancel the ct.ThrowIfCancellationRequested()
-                // will throw and the task will transition to the canceled state.
-                FCts[index].Cancel();
-
-                // Dispose the cancellation token source and set it to null so we know
-                // to setup a new one in a next frame.
-                FCts[index].Dispose();
-                FCts[index] = null;
-            }
-        }
     }
 
     #region PluginInfo
@@ -163,10 +114,12 @@ namespace VVVV.Nodes
         [Input("Text", DefaultString = "Your Message")]
         public IDiffSpread<string> FTextMessage;
 
-        protected override async void sendMessage(int i)
+        protected override async Task sendMessageAsync(int i)
         {
-            FTask[i] = Task<Message>.Factory.StartNew(() => FClient[i].BC.SendTextMessageAsync(FChatId[i], FTextMessage[i], false, false, 0, null, ParseMode.Default).Result);
-            //await FClient[i].BC.SendTextMessageAsync(FChatId[i], FTextMessage[i], false, false, 0, null, ParseMode.Default);
+            //FTask[i] = Task<Message>.Factory.StartNew(() => FClient[i].BC.SendTextMessageAsync(FChatId[i], FTextMessage[i], false, false, 0, null, ParseMode.Default).Result);
+            await FClient[i].BC.SendTextMessageAsync(FChatId[i], FTextMessage[i], false, false, 0, null, ParseMode.Default);
+            FStopwatch[i].Stop();
+            FSent[i] = true;    // TODO: only do when sending was actually successful
         }
 
     }
@@ -182,20 +135,18 @@ namespace VVVV.Nodes
         [Input("FCaption", DefaultString = "caption")]
         public IDiffSpread<string> FCaption;
 
-        protected override async void sendMessage(int i)
+        protected override async Task sendMessageAsync(int i)
         {
 
             await FClient[i].BC.SendChatActionAsync(FChatId[i], ChatAction.UploadPhoto);
             
-            // original example code
             using (var fileStream = new FileStream(FFileName[i], FileMode.Open, FileAccess.Read, FileShare.Read))
             {
                 var fts = new FileToSend(FFileName[i], fileStream);
 
-                // Does not work - Cannot read from File that is not opened. (threading is probably the reason)
-                //FTask[i] = Task<Message>.Factory.StartNew(() => FClient[i].BC.SendPhotoAsync(FChatId[i], fts, FCaption[i]).Result);
-
                 await FClient[i].BC.SendPhotoAsync(FChatId[i], fts, FCaption[i]);
+                FStopwatch[i].Stop();
+                FSent[i] = true;    // TODO: only do when sending was actually successful
 
                 // Exception when trying to access the stream from vvvv directly
                 // Exception: Bytes to be written to the stream exceed the Content-Length bytes size specified
