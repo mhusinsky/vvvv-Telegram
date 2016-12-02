c27184188f47ddbde742e7e30f44dcafd5dabe60
Matthias Husinsky <mhusinsky@fhstp.ac.at>
2016-12-02 23:05:05 +0100

    first commit of callback node

---
+++ vvvv-Telegram/TelegramCallbackNodes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.ComponentModel.Composition;
+
+using System.Diagnostics;
+
+using VVVV.PluginInterfaces.V2;
+using VVVV.Core.Logging;
+using VVVV.PluginInterfaces.V1;
+using VVVV.Utils;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+namespace VVVV.Nodes
+{
+    public abstract class TelegramCallbackNode : IPluginEvaluate
+    {
+        #region fields & pins
+        [Input("Bots")]
+        public ISpread<BotClient> FBotClient;
+
+        [Output("Received", IsBang = true)]
+        public ISpread<bool> FReceived;
+
+        [Import()]
+        public ILogger FLogger;
+        #endregion fields & pins
+
+        public void Evaluate(int SpreadMax)
+        {
+            setCallbacksSliceCount(FBotClient.SliceCount);
+            
+            for (int i = 0; i < FBotClient.SliceCount; i++)
+            {
+                FReceived[i] = false;
+                if (FBotClient[i] == null)
+                    return;
+                checkForCallback(i);
+            }
+
+        }
+
+        protected void setCallbacksSliceCount(int botCount)
+        {
+            FReceived.SliceCount = botCount;
+        }
+
+        protected void checkForCallback(int i)
+        {
+            var callbacks = FBotClient[i].Callbacks;
+
+            foreach (VTelegramCallback cb in callbacks)
+            {
+
+            }
+        }
+
+
+    }
+}
